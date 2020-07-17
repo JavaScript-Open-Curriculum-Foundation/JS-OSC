62710345027bfce91b5d069476e83671faf8ca91
Hans <bmm79@nau.edu>
2020-07-17 10:24:07 -0700

    reorder the sections
    Trying to make things more organized :-/

---
+++ JS/02-DataTypes-and-Debugging/debugger.js
@@ -0,0 +1,5 @@
+// 3 Breaking your code intentionally
+// using the debugger, stops the execution of JavaScript and may also call debugging function. This one way we can set a breakpoint in JS.
+if(EXTERMINATOR) debugger;
+
+
