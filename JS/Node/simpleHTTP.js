38207af27fd20d57883ffdaf9b00002c3e396172
Hans <bmm79@nau.edu>
2020-07-20 13:47:28 -0700

    Add simple example of simple node server.

---
+++ JS/Node/simpleHTTP.js
@@ -0,0 +1,52 @@
+/**
+ * 1. Pass a video into the media file.
+ * - https://www.pexels.com/videos/
+ * 2. Use puppeteer to screen shot the video. 
+ * 3. https://stackoverflow.com/questions/28822034/simple-node-js-server-that-sends-htmlcss-as-response
+ */
+
+// import http from 'http';
+let http = require('http')
+const fs = require('fs');
+
+const PORT = 7000;
+
+
+/**
+ * @description: accepts three template strings as arguments and return html.
+ */
+const html = (header,body,footer) => {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    ${header}
+  </head>
+  <body>
+  <h1>${body}</h1>
+  ${footer}
+  </body>
+  </html>`;
+}
+
+
+http.createServer(
+  (req,res)=>{
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': html.length,
+      'Expires': new Date().toUTCString()
+    })
+    res.end(
+      html(
+        `<link rel="stylesheet" href="/public/style.css">`,
+        `<h1>HELLO</H1>`,
+        `<script src="/public/alert.js"></script>`
+      ) 
+    );
+  }
+).listen(PORT);
+
+
